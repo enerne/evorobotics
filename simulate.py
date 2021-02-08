02936e64cdf55ff937248e83f9c499cae867450c
Ethan Nerney <enerney@uvm.edu>
2021-02-08 14:02:46 -0500

    generate

--- simulate.py
+++ simulate.py
@@ -5,7 +5,6 @@ physicsClient = p.connect(p.GUI)
 
 for i in range(0,1000):
     p.stepSimulation()
-    time.sleep(1/60)
-    print("step")
-
+    time.sleep(1.0/60.0)
+print("disconnecting")
 p.disconnect()
